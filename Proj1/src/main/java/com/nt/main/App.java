21eeaa58cddc1aae528018d42ba35446e66bf185
Admin <Admin@DESKTOP-7TDQ3RQ>
2024-11-08 23:29:20 +0530

    Dev1 wrote code in App.java and AppTest.java and performed unit testing

--- Proj1/src/main/java/com/nt/main/App.java
+++ Proj1/src/main/java/com/nt/main/App.java
@@ -1,10 +1,15 @@
 package com.nt.main;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+public class App 
+{
+	public int sum(int x, int y) 
+	{
+		return x + y;
+	}
+    public static void main(String[] args) 
+    {
+        App app = new App();
+        app.sum(10, 20);
     }
 }
--- Proj1/src/test/java/com/nt/main/AppTest.java
+++ Proj1/src/test/java/com/nt/main/AppTest.java
@@ -1,19 +1,37 @@
 package com.nt.main;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumWithPositives() 
+    {
+    	App app = new App();
+    	int expected = 30;
+    	int actual = app.sum(10, 20);
+    	assertEquals(expected, actual);
+    	
+    }
+    @Test
+    public void testSumWithNegatives() 
+    {
+    	App app = new App();
+    	int expected = -30;
+    	int actual = app.sum(-10, -20);
+    	assertEquals(expected, actual);
+    	
+    }
+    @Test
+    public void testSumWithMixedValues() 
+    {
+    	App app = new App();
+    	int expected = -10;
+    	int actual = app.sum(10, -20);
+    	assertEquals(expected, actual);
+    	
     }
 }
